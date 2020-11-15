c83bda91f1fe670c43f631e3d64f8102985adf23
karthik <karthikreddyjannupalli@gmail.com>
2020-11-15 14:19:34 +0530

    added code editor

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,53 +1,54 @@
-import React from "react"
-import {GoogleLoginButton} from "react-social-login-buttons"
-function SignUp(){
-    return(
-    <div class="page">
-               <div className="signel"><div className="btnbox"><span onClick={login}>Login</span>
-               <span onClick={register}>Register</span>
-               <hr id="ind"></hr></div>
-        <form id="login">
-        <div><input placeholder="username" type="username"/></div>
-        <div><input placeholder="password" type="password"/></div>
-        <div className="gbut"><GoogleLoginButton style={{width:200},{backgroundColor:"#db4437"}} activeStyle={{backgroundColor:"#3b5998"}}/></div>
-      </form>
-       <form id="register">
-            <input type="text"placeholder="Name"/>
-            <br/>
-            <input type="number"placeholder="Roll Number"/>
-            <br/>
-            <input type="text"placeholder="Branch"/>
-            <br/>
-            <input type="text"placeholder="Section"/>
-        <div><input placeholder="username" type="username"/></div>
-        <div><input placeholder="password" type="password"/></div>
-    <input type="checkbox"/><label>Student</label>
-    <input type="checkbox"/><label>Faculty</label>
-    <div className="grbut"><GoogleLoginButton className="reg"text="Register with Google"style={{width:230,backgroundColor:"#db4437"}} activeStyle={{backgroundColor:"#3b5998"}}/></div>
-</form>
-       </div>
-      
-           </div>
-    )
+import React, { useState } from "react";
+import { CardHeader, Col, Label, Row } from "reactstrap";
+import { GoogleLoginButton } from "react-social-login-buttons";
+import { Tab, Tabs } from "react-bootstrap";
+import Card from '@material-ui/core/Card';
+import "./signup.css";
+import { GoogleLogin } from 'react-google-login';
+import { Control, Errors, Form } from "react-redux-form";
+import { Button } from "@material-ui/core";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
+function ControlledTabs() {
+  const [key, setKey] = useState("Login");
+
+  return (
+    <Tabs
+      id="sign"
+      fill
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+      variant="pills"
+    >
+      <Tab eventKey="Login" title="Login">
+        <div className="container" style={{width: "200px"}}>
+            
+        </div>
+      </Tab>
+      <Tab eventKey="signup" title="Signup"></Tab>
+    </Tabs>
+  );
+}
+function SignUp() {
+  return (
+    <div className="container">
+      <br />
+      <br />
+      <div className="row">
+        <div className="offset-md-6 col-5">
+          <Card>
+            <ControlledTabs />
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUp;
-function login(){
-    var logform=document.getElementById("login");
-    var regform=document.getElementById("register");
-    var indicator=document.getElementById("ind")
-    logform.style.transform="translateX(0px)"   
-    indicator.style.transform="translateX(-43px)"    
-    regform.style.transform="translateX(0px)"  
-    indicator.style.marginLeft="49px"
-}
-function register(){
-    var logform=document.getElementById("login");
-    var indicator=document.getElementById("ind")
-
-    var regform=document.getElementById("register");
-    logform.style.transform="translateX(340px)"
-    regform.style.transform="translateX(340px)"
-    indicator.style.transform="translateX(34px)" 
-    indicator.style.marginLeft="43px"
-}
